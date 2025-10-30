ff2754284e4cfeb142e00046a6ed07bc0963d215
ceriosto <rtxrios21@gmail.com>
2025-10-30 12:23:59 -0500

    Update xor_cipher.py

--- xor_cipher.py
+++ xor_cipher.py
@@ -1,1 +1,14 @@
-
+import random
+def xor(texto, clave):
+    resultado = ""
+    for i in range(len(texto)):
+        resultado += chr(ord(texto[i]) ^ ord(clave[i % len(clave)]))
+    return resultado
+texto = input("Escribe el texto a cifrar: ")
+caracteres = "01"
+llave = ''.join(random.choice(caracteres) for _ in range(len(texto)))
+print("Llave generada:", llave)
+cifrado = xor(texto, llave)
+print("Texto cifrado:", cifrado)
+descifrado = xor(cifrado, llave)
+print("Texto descifrado:", descifrado)
